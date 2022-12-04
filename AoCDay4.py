749d112ac2d58b5d7b8cf728cbfeb6c9f4aa4eaa
Emil Jonsson <83821125+Jonzoe@users.noreply.github.com>
2022-12-04 22:19:46 +0100

    Saknar part2
    :skull:

---
+++ AoCDay4.py
@@ -0,0 +1,34 @@
+with open("Day4.txt") as file:
+    data = [i for i in file.read().splitlines()]
+
+def part1(): 
+    pairs = []
+
+    for i in data:
+        x = i.split(",")
+
+        pairs.append(x)
+
+    nissequeue = []
+
+    for i in pairs:
+        for j in i:
+            x = j.split("-")
+
+            specific_nisse = (int(x[0]), int(x[1]))
+
+
+            nissequeue.append(specific_nisse)
+
+    answer = 0
+
+    for i in range(0, (len(nissequeue)-1), 2):
+        specific_nisse = nissequeue[i]
+        next_nisse = nissequeue[i+1]
+
+        if (next_nisse[0] >= specific_nisse[0] and next_nisse[1] <= specific_nisse[1]) or (specific_nisse[0] >= next_nisse[0] and specific_nisse[1] <= next_nisse[1]):
+            answer += 1
+        
+    return(answer)
+
+print(part1())
